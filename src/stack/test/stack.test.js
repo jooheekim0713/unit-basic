fc17e11a9778fbe9628c06b8f165643b6a5bf758
jooheekim <jooheekim.dev@gmail.com>
2023-08-08 17:02:32 +0900

    Test : write test code using stack

---
+++ src/stack/test/stack.test.js
@@ -0,0 +1,49 @@
+const Stack = require('../stack');
+
+describe('Stack', () => {
+  let stack;
+
+  beforeEach(() => {
+    stack = new Stack();
+  });
+
+  it('is created empty', () => {
+    expect(stack.size()).toBe(0);
+  });
+
+  it('allows to push item', () => {
+    stack.push('😀');
+    expect(stack.size()).toBe(1);
+  });
+
+  describe('pop', () => {
+    it('throws an error if stack is empty', () => {
+      expect(() => {
+        stack.pop();
+      }).toThrow('Stack is empty');
+    });
+
+    it('returns the last pushed item and remove it from the stack', () => {
+      stack.push('😀');
+      stack.push('😁');
+
+      expect(stack.pop()).toBe('😁');
+      expect(stack.size()).toBe(1);
+    });
+  });
+
+  describe('peek', () => {
+    it('throws an error if stack is empty', () => {
+      expect(() => {
+        stack.peek();
+      }).toThrow('Stack is empty');
+    });
+    it('returns the last pushed item but keep it from the stack', () => {
+      stack.push('😀');
+      stack.push('😁');
+
+      expect(stack.peek()).toBe('😁');
+      expect(stack.size()).toBe(2);
+    });
+  });
+});
